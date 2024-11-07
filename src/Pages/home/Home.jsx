321f94577be4e133e26622704f71c31672fcb365
abdulkader3 <abdulkader.wabdeveloper@gmail.com>
2024-11-07 15:47:06 +0600

    pin setup in realtime database is done.

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,62 +1,117 @@
 import React, { useState } from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import { IoChevronBackOutline } from 'react-icons/io5'
-import { LuLogOut } from 'react-icons/lu'
+import { IoChevronBackOutline } from "react-icons/io5";
+import { LuLogOut } from "react-icons/lu";
 import { useSelector } from "react-redux";
-
+import { getAuth, EmailAuthProvider, reauthenticateWithCredential } from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
 
 const Home = () => {
+  const clintInfo = useSelector((state) => state.info.userdata);
 
+  const [one, tow] = useState(false);
+  const [password, setPassword] = useState("");
+  const [pin, setPin] = useState("");
+  const auth = getAuth();
 
-  // rudux 
-  const clintInfo = useSelector((state)=>state.info.userdata)
+  const ProfileNext = () => {
+    tow(!one);
+  };
 
+  const logout = () => {
+    localStorage.clear();
+    location.reload();
+  };
 
-  const [one , tow ] = useState(false)
+  // Function to reauthenticate user
+  const reauthenticateUser = async () => {
+    const user = auth.currentUser;
+    const credential = EmailAuthProvider.credential(user.email, password);
+    try {
+      await reauthenticateWithCredential(user, credential);
+      return true;
+    } catch (error) {
+      console.error("Incorrect password", error);
+      return false;
+    }
+  };
 
-  const ProfileNext = ()=>{
-   tow(!one)
-  }
-
-
-  const logout = ()=>{
-   localStorage.clear()
-   location.reload()
-  }
- 
+  // Function to set PIN in Realtime Database
+  const handleSetPin = async () => {
+    const isAuthenticated = await reauthenticateUser();
+    if (isAuthenticated) {
+      const db = getDatabase();
+      await set(ref(db, `ClintList/${auth.currentUser.uid}/pin`), pin);
+      alert("PIN set successfully!");
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  };
 
   return (
     <>
       <div className=" homePage ">
-
-
-
-      <div className="w-full relative">
-      {/* top */}
-     <div onClick={ProfileNext} className=" homeNav w-full h-[60px] md:h-[100px] bg-white flex items-center justify-between p-5 md:p-10 lg:p-20 ">
-        <div className=" border py-2 px-4 rounded-full ">00.00TK</div>
-        <h2>{clintInfo?.displayName} </h2>
-        <img className='w-[30px] md:w-[70px] lg:w-[100px] overflow-hidden rounded-full ' src={clintInfo?.photoURL} alt="profile" />
-      </div>
-      {/* top */}
-
-      {
-        one && <div className="w-full h-screen bg-transparent ProfilePage absolute z-10 ">
-          <div className="w-full text-[30px] ">
-            <IoChevronBackOutline onClick={ProfileNext} />
+        <div className="w-full relative">
+          {/* top */}
+          <div
+            onClick={ProfileNext}
+            className=" homeNav w-full h-[60px] md:h-[100px] bg-white flex items-center justify-between p-5 md:p-10 lg:p-20 "
+          >
+            <div className=" border py-2 px-4 rounded-full ">00.00TK</div>
+            <h2>{clintInfo?.displayName} </h2>
+            <img
+              className="w-[30px] md:w-[70px] lg:w-[100px] overflow-hidden rounded-full "
+              src={clintInfo?.photoURL}
+              alt="profile"
+            />
           </div>
+          {/* top */}
 
-          <div onClick={logout} className=" flex items-center gap-1 justify-end ">
-          <LuLogOut className=' rotate-180' /> <button>  LOgOut </button>
-          </div>
+          {one && (
+            <div className="w-full h-screen bg-transparent ProfilePage absolute z-10 ">
+              <div className="w-full text-[30px] ">
+                <IoChevronBackOutline onClick={ProfileNext} />
+              </div>
 
+              {/* Password input */}
+              <div className="w-full h-[40px] rounded-full ">
+                <input
+                  className="w-full h-full rounded-full text-black pl-5 "
+                  type="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              {/* PIN input */}
+              <div className="w-full h-[40px] rounded-full my-5 ">
+                <input
+                  className="w-full h-full rounded-full text-black pl-5"
+                  type="text"
+                  placeholder="Enter PIN"
+                  value={pin}
+                  onChange={(e) => setPin(e.target.value)}
+                />
+              </div>
+
+              {/* Set PIN button */}
+              <button onClick={handleSetPin} className="w-full mt-2 bg-blue-500 text-white rounded-full py-2">
+                Set PIN
+              </button>
+
+              <div
+                onClick={logout}
+                className=" flex items-center justify-center text-black  "
+              >
+                <div className=" w-[100px] flex items-center gap-1 bg-white h-[40px] rounded-full justify-center mt-20  ">
+                <LuLogOut className=" rotate-180" /> <button> Log Out </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-      }
-     </div>
-
-
-
 
         {/* option part */}
         <div className=" optionPart w-full  flex mt-2 p-5 flex-wrap gap-4 text-[12px] font-bold ">
@@ -105,7 +160,10 @@ const Home = () => {
             />{" "}
             Balance{" "}
           </Link>
-          <Link className="flex ml-4 flex-col items-center" to="/transferminute">
+          <Link
+            className="flex ml-4 flex-col items-center"
+            to="/transferminute"
+          >
             {" "}
             <img
               className=" w-[30px] overflow-hidden rounded-xl "
@@ -146,6 +204,7 @@ const Home = () => {
         {/* option part */}
 
         {/* history */}
+        
         <div className=" w-full h-[250px] md:h-[300px]  overflow-y-scroll History py-5 px-2 md:px-5 lg:px-10  flex flex-wrap gap-5  ">
           <div className="w-full text-[12px] md:text-[18px] h-[40px] bg-green-400 flex items-center justify-between p-3 rounded-xl ">
             <p> 019XXXXXXXXX </p>
